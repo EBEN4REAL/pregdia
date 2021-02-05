88f2f17bbdd14cbb0c6672e2f8d2a94105df0f4e
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-02-05 12:21:02 +0100

    ..

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.querySelector('.card-1').addEventListener('click' , (e) => {
-    localStorage.setItem('pregiaCategory', 'prenatal')
-    // setTimeout(() => {
-    //     window.location.replace("http://127.0.0.1:5500/diagnose.html")
-    // }, 3000)
+    localStorage.setItem('pregiaCategory', 'pre-natal')
+    setTimeout(() => {
+        window.location.replace("http://127.0.0.1:5500/diagnose.html")
+    }, 3000)
     document.querySelector('.card-1').style.boxShadow = '0px 4.5px 7px #6757a8'
     document.querySelector('.card-2').style.boxShadow = '0px 1.5px 3px #0000000d'
     document.querySelector('.header-bg').style.background = "linear-gradient(rgb(0, 0, 0, 0.8), rgb(0, 0, 0, 0.8)),  url(img/preg.jpeg) no-repeat"
@@ -13,10 +13,10 @@ document.querySelector('.card-1').addEventListener('click' , (e) => {
     document.querySelector('.header-bg-2').style.backgroundPosition = "center"
 })
 document.querySelector('.card-2').addEventListener('click' , (e) => {
-    localStorage.setItem('pregiaCategory', 'postnatal')
-    // setTimeout(() => {
-    //     window.location.replace("http://127.0.0.1:5500/diagnose.html")
-    // }, 3000)
+    localStorage.setItem('pregiaCategory', 'post-natal')
+    setTimeout(() => {
+        window.location.replace("http://127.0.0.1:5500/diagnose.html")
+    }, 3000)
     document.querySelector('.card-2').style.boxShadow = '0px 4.5px 7px #6757a8'
     document.querySelector('.card-1').style.boxShadow = '0px 1.5px 3px #0000000d'
     document.querySelector('.header-bg').style.background = "linear-gradient(rgb(0, 0, 0, 0.8), rgb(0, 0, 0, 0.8)),  url(img/pn.jpeg) no-repeat"
@@ -31,3 +31,7 @@ document.querySelector('.card-2').addEventListener('click' , (e) => {
 function goBack($route) {
     window.location.replace(`http://127.0.0.1:5500/${$route}.html`)
 }
+
+
+
+
